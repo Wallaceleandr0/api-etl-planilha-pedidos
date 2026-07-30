ae8b57d139bb549463a18385b5ffce9b591cb1ac
Wallace Leandro <wallaceleandro2003@gmail.com>
2026-07-29 22:31:31 -0300

    url analise-pedidos-web adicionada em origins

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI, HTTPException, UploadFile, File
 from tratar_dados import tratar_planilha_saldo
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://analise-pedidos-web.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post('/tratar')
 async def dados(arquivo: UploadFile = File(...)):
